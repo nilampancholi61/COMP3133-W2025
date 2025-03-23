ca0db5c1c23df0bf52a7ec94cacf22ea672487a0
nilampancholi61 <155118869+nilampancholi61@users.noreply.github.com>
2025-03-23 16:27:47 -0400

    added

--- Lab8/my-angular-project/src/app/input-format.directive.ts
+++ Lab8/my-angular-project/src/app/input-format.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
+  @HostListener('blur') onBlur() {
+    this.formatInput();
+  }
+
+  private formatInput() {
+    const inputValue = this.el.nativeElement.value;
+    this.el.nativeElement.value = inputValue.toUpperCase();
+  }
 }
+
